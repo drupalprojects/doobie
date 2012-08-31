470c6115636c48b274b192b5f3137eca6500eba6
Jonathan Hedstrom <jhedstrom@gmail.com>
2012-08-30 18:18:32 -0700

    Round two attempt to swap in Drupal Extension.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -23,8 +23,9 @@ abstract class HackyDataRegistry {
   }
 }
 
-use Behat\MinkExtension\Context\MinkContext,
-    Behat\Behat\Exception\PendingException;
+use Behat\Behat\Exception\PendingException,
+    Behat\Gherkin\Node\TableNode;
+use Drupal\DrupalExtension\Context\DrupalContext;
 use Symfony\Component\Process\Process;
 
 use Behat\Gherkin\Node\TableNode;
@@ -41,7 +42,7 @@ require 'vendor/autoload.php';
 /**
  * Features context.
  */
-class FeatureContext extends MinkContext {
+class FeatureContext extends DrupalContext {
 
   /**
    * Current authenticated user.
@@ -49,16 +50,6 @@ class FeatureContext extends MinkContext {
    * A value of FALSE denotes an anonymous user.
    */
   public $user = FALSE;
-
-  /**
-   * Keep track of all users that are created so they can easily be removed.
-   */
-  private $users = array();
-
-  /**
-   * Store a drush alias for tests requiring shell access.
-   */
-  private $drushAlias = FALSE;
 
   /**
    * Store region ids.
@@ -106,13 +97,7 @@ class FeatureContext extends MinkContext {
    *   Context parameters (set them up through behat.yml or behat.local.yml).
    */
   public function __construct(array $parameters) {
-    if (isset($parameters['basic_auth'])) {
-      $this->basic_auth = $parameters['basic_auth'];
-    }
     $this->default_browser = $parameters['default_browser'];
-    if (isset($parameters['drush_alias'])) {
-      $this->drushAlias = $parameters['drush_alias'];
-    }
     if (isset($parameters['drupal_users'])) {
       $this->drupal_users = $parameters['drupal_users'];
     }
@@ -127,57 +112,6 @@ class FeatureContext extends MinkContext {
     }
     if (isset($parameters['post title'])) {
       $this->postTitle= $parameters['post title'];
-    }
-  }
-
-  /**
-   * Run before every scenario.
-   *
-   * @BeforeScenario
-   */
-  public function beforeScenario($event) {
-    if (isset($this->basic_auth)) {
-      $driver = $this->getSession()->getDriver();
-      if ($driver instanceof Behat\Mink\Driver\Selenium2Driver) {
-        // Continue if this is a Selenium driver, since this is handled in
-        // locatePath().
-      }
-      else {
-        // Setup basic auth.
-        $this->getSession()->setBasicAuth($this->basic_auth['username'], $this->basic_auth['password']);
-      }
-    }
-  }
-
-  /**
-   * Check for shell access (via drush).
-   *
-   * @BeforeScenario @shellAccess
-   */
-  public function checkShellAccess() {
-    // @todo check that this is a functioning alias.
-    // See http://drupal.org/node/1615450
-    if (!$this->drushAlias) {
-      throw new pendingException('This scenario requires shell access.');
-    }
-  }
-
-  /**
-   * Run after every scenario.
-   *
-   * @AfterScenario
-   */
-  public function afterScenario($event) {
-    // Remove any users that were created.
-    if (!empty($this->users)) {
-      foreach ($this->users as $user) {
-        $process = new Process("drush @{$this->drushAlias} user-cancel --yes {$user->name} --delete-content");
-        $process->setTimeout(3600);
-        $process->run();
-        if (!$process->isSuccessful()) {
-          throw new RuntimeException($process->getErrorOutput());
-        }
-      }
     }
   }
 
@@ -204,47 +138,6 @@ class FeatureContext extends MinkContext {
    * @{
    */
   private $random = array();
-
-  /**
-   * Helper function to generate a random string of arbitrary length.
-   *
-   * Copied from drush_generate_password().
-   *
-   * @param int $length
-   *   Number of characters the generated string should contain.
-   * @param string $store
-   *   The name to store this random string in for later retrieval with fetchRandomString()
-   *
-   * @return string
-   *   The generated string.
-   */
-  public function randomString($length = 10, $store = FALSE) {
-    // This variable contains the list of allowable characters for the
-    // password. Note that the number 0 and the letter 'O' have been
-    // removed to avoid confusion between the two. The same is true
-    // of 'I', 1, and 'l'.
-    $allowable_characters = 'abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789';
-
-    // Zero-based count of characters in the allowable list:
-    $len = strlen($allowable_characters) - 1;
-
-    // Declare the password as a blank string.
-    $pass = '';
-
-    // Loop the number of times specified by $length.
-    for ($i = 0; $i < $length; $i++) {
-
-      // Each iteration, pick a random character from the
-      // allowable string and append it to the password:
-      $pass .= $allowable_characters[mt_rand(0, $len)];
-    }
-
-    if ($store) {
-      $this->random[$store] = $pass;
-    }
-
-    return $pass;
-  }
 
   /**
    * Helper function to fetch previously generated random strings stored by randomString().
@@ -311,19 +204,6 @@ class FeatureContext extends MinkContext {
   }
 
   /**
-   * Determine if the a user is already logged in.
-   */
-  public function loggedIn() {
-    $session = $this->getSession();
-    $session->visit($this->locatePath('/'));
-
-    // If a logout link is found, we are logged in. While not perfect, this is
-    // how Drupal SimpleTests currently work as well.
-    $element = $session->getPage();
-    return $element->findLink('Log out');
-  }
-
-  /**
    * Private function for the whoami step.
    */
   private function whoami() {
@@ -351,158 +231,11 @@ class FeatureContext extends MinkContext {
    */
 
   /**
-   * @Given /^(?:that I|I) am at "([^"]*)"$/
-   */
-  public function iAmAt($path) {
-    // Use the Mink Extenstion step definition.
-    return new Given("I am on \"$path\"");
-  }
-
-  /**
    * @Given /^(?:that I|I) am (?:on|at) the homepage$/
    */
   public function thatIAmOnTheHomepage() {
     // Use the Mink Extenstion step definition.
     return new Given("I am on homepage");
-  }
-
-
-  /**
-   * @When /^I visit "([^"]*)"$/
-   */
-  public function iVisit($path) {
-    // Use the Mink Extenstion step definition.
-    return new Given("I am on \"$path\"");
-  }
-
-
-  /**
-   * @When /^I click "([^"]*)"$/
-   */
-  public function iClick($linkname) {
-    // Use the Mink Extenstion step definition.
-    return new Given("I follow \"$linkname\"");
-  }
-
-  /**
-   * @Given /^for "([^"]*)" I enter "([^"]*)"$/
-   * @Given /^I enter "([^"]*)" for "([^"]*)"$/
-   */
-  public function forIenter($fieldname, $formvalue) {
-    // Use the Mink Extenstion step definition.
-    return new Given("I fill in \"$fieldname\" with \"$formvalue\"");
-  }
-
-  /**
-   * @When /^I press the "([^"]*)" button$/
-   */
-  public function iPressTheButton($button) {
-    // Use the Mink Extenstion step definition.
-    return new Given("I press \"$button\"");
-  }
-
-  /**
-   * @Then /^I (?:|should )see the link "([^"]*)"$/
-   */
-  public function iShouldSeeTheLink($linkname) {
-    $element = $this->getSession()->getPage();
-    $result = $element->findLink($linkname);
-    if (empty($result)) {
-      throw new Exception("No link to " . $linkname . " on " . $this->getSession()->getCurrentUrl());
-    }
-  }
-
-  /**
-   * @Then /^I (?:should|do) not see the link "([^"]*)"$/
-   */
-  public function iShouldNotSeeTheLink($linkname) {
-    $element = $this->getSession()->getPage();
-    $result = $element->findLink($linkname);
-    if ($result) {
-      throw new Exception("The link " . $linkname . " was present on " . $session->getCurrentUrl() . " and was not supposed to be.");
-    }
-  }
-
-  /**
-   * @Then /^I (?:|should )see the heading "([^"]*)"$/
-   */
-  public function iShouldSeeTheHeading($headingname) {
-    $element = $this->getSession()->getPage();
-    foreach (array('h1', 'h2', 'h3', 'h4', 'h5', 'h6') as $heading) {
-      $results = $element->findAll('css', $heading);
-      foreach ($results as $result) {
-        if ($result->getText() == $headingname) {
-          return;
-        }
-      }
-    }
-    throw new Exception("The text " . $headingname . " was not found in any heading " . $this->getSession()->getCurrentUrl());
-  }
-
-  /**
-   * @Then /^I (?:|should )see the text "([^"]*)"$/
-   */
-  public function iShouldSeeTheText($text) {
-    // Use the Mink Extension step definition.
-    return new Given("I should see text matching \"$text\"");
-  }
-
-  /**
-   * @Then /^I should not see the text "([^"]*)"$/
-   */
-  public function iShouldNotSeeTheText($text) {
-    // Use the Mink Extension step definition.
-    return new Given("I should not see text matching \"$text\"");
-  }
-
-  /**
-   * @Then /^I should get a "([^"]*)" HTTP response$/
-   */
-  public function iShouldGetAHttpResponse($status_code) {
-    // Use the Mink Extension step definition.
-    return new Given("the response status code should be $status_code");
-  }
-
-  /**
-   * @Then /^I should not get a "([^"]*)" HTTP response$/
-   */
-  public function iShouldNotGetAHttpResponse($status_code) {
-    // Use the Mink Extension step definition.
-    return new Given("the response status code should not be $status_code");
-  }
-
-  /**
-   * @Given /^I check the box "([^"]*)"$/
-   */
-  public function iCheckTheBox($checkbox) {
-    // Use the Mink Extension step definition.
-    return new Given("I check \"$checkbox\"");
-  }
-
-  /**
-   * @Given /^I uncheck the box "([^"]*)"$/
-   */
-  public function iUncheckTheBox($checkbox) {
-    // Use the Mink Extension step definition.
-    return new Given("I uncheck \"$checkbox\"");
-  }
-
-  /**
-   * @When /^I select the radio button "([^"]*)" with the id "([^"]*)"$/
-   * @TODO convert to mink extension.
-   */
-  public function iSelectTheRadioButtonWithTheId($label, $id) {
-    $element = $this->getSession()->getPage();
-    $radiobutton = $element->findById($id);
-    if ($radiobutton === NULL) {
-      throw new Exception('Neither label nor id was found');
-    }
-    $value = $radiobutton->getAttribute('value');
-    $labelonpage = $radiobutton->getParent()->getText();
-    if ($label != $labelonpage) {
-      throw new Exception("Button with $id has label $labelonpage instead of $label.");
-    }
-    $radiobutton->selectOption($value, FALSE);
   }
 
   /**
@@ -603,64 +336,6 @@ class FeatureContext extends MinkContext {
    */
 
   /**
-   * @Given /^I am an anonymous user$/
-   */
-  public function iAmAnAnonymousUser() {
-    // Verify the user is logged out.
-    if ($this->loggedIn()) {
-      $this->logout();
-    }
-  }
-
-  /**
-   * Creates and authenticates a user with the given role via Drush.
-   *
-   * @Given /^I am logged in as a user with the "([^"]*)" role$/
-   */
-  public function iAmLoggedInWithRole($role) {
-    // Check if a user with this role is already logged in.
-    if ($this->user && isset($this->user->role) && $this->user->role == $role) {
-      return TRUE;
-    }
-
-    // Create user (and project)
-    $name = $this->randomString(8);
-    $pass = $this->randomString(16);
-
-    // Create a new user.
-    $process = new Process("drush @{$this->drushAlias} user-create --password={$pass} --mail=$name@example.com $name");
-    $process->setTimeout(3600);
-    $process->run();
-    if (!$process->isSuccessful()) {
-      throw new RuntimeException($process->getErrorOutput());
-    }
-
-    $this->users[] = $this->user = (object) array(
-      'name' => $name,
-      'pass' => $pass,
-      'role' => $role,
-    );
-
-    if ($role == 'authenticated user') {
-      // Nothing to do.
-    }
-    else {
-      // Assign the given role.
-      $process = new Process("drush @{$this->drushAlias} user-add-role \"{$role}\" {$name}");
-      $process->setTimeout(3600);
-      $process->run();
-      if (!$process->isSuccessful()) {
-        throw new RuntimeException($process->getErrorOutput());
-      }
-    }
-
-    // Login.
-    $this->login();
-
-    return TRUE;
-  }
-
-  /**
    * Authenticates a user.
    *
    * @Given /^I am logged in as "([^"]*)" with the password "([^"]*)"$/
@@ -683,6 +358,7 @@ class FeatureContext extends MinkContext {
     $this->getSession()->visit($this->locatePath('/user'));
     // Get the page title.
     $page_title = $element->findByID('page-title')->getText();
+
     if ($page_title == 'User account') {
       // If I see this, I'm not logged in at all so log in.
       $element->fillField('Username', $username);
