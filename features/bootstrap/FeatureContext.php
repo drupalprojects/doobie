3de8dbfbc1c04f931fe2ff532d695c9785674145
Melissa Anderson <melissa@happypunch.com>
2012-09-25 06:35:03 -0700

    Issue #1706714 by eliza411: Removed the empty stub for the breadcrumb step definition
    Signed-off-by: Melissa Anderson <melissa@happypunch.com>

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1762,13 +1762,6 @@ class FeatureContext extends DrupalContext {
   }
 
   /**
-   * @Then /^I should see the breadcrumb "([^"]*)"$/
-   */
-  public function iShouldSeeTheBreadcrumb($breadcrumb) {
-    throw new PendingException();
-  }
-
-  /**
    * @When /^I follow a post$/
    * Function to get the link from a table's first row
    */
